8de9c648044ab48caa14c5393b2839485e572d69
geoffrey woodcock <theatomproject@gmail.com>
2018-01-27 18:45:04 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+function accessElementInArray(array, index){
+  return array[2];
+}
+var chocolateBars = ["snickers", "hundred grand", "kitkst", "skittles"]
+function destructivelyAddElementToBeginningOfArray(array, element){
+   array.unshift(element);
+   return array
+}
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
+}
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
+  array.shift(element)
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1);
+
+}
